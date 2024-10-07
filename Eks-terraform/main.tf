8a7299657faddb637a6b29f53e358c6131e27f29
Manjunath Kakkepalya Puttaswamy <85114693+MadMaxGit@users.noreply.github.com>
2024-10-07 16:19:17 +0200

    dkdkjasnkja

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = [data.aws_subnets.public.id[0]]
+    subnet_ids = [data.aws_subnets.public.values[0]]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
